e14918a6277ea5ea84915ddc82465739c24a511c
Kara Capozzi <karacapozzi@Karas-MacBook.local>
2017-09-06 19:07:34 -0400

    add hash method

--- hash_reinforcement.rb
+++ hash_reinforcement.rb
@@ -12,42 +12,58 @@ otherwise the value should be the same number as the key
 4. store the keys and values in a Hash
 
 
-def divisible_by_2_hash(key_num)
-  if key_num %2 == 0
-    return value_num {}<< key_num + 1
+def divisible_hash(key_num)
+  key_num = {}
+
+if key_num %2 == 0 && key_num %7 == 0
+  return key_num * 2
+
+  elsif key_num %2 == 0
+    return key_num + 1
+
+  elsif key_num %7 == 0
+    return key_num - 1
+
 end
 
 
-def divisible_by_7_hash
 
-end
-
-def divisible_by_2_and_7_hash
-end
-
-
-{
-1: 1,
-2: 3,
-3: 3,
-4: 5,
-5: 5,
-6: 7,
-7: 6,
-8: 9,
-9: 9,
-10: 11,
-11: 11,
-12: 13,
-13: 13,
-14: 28,
-15: 15,
-16: 17,
-17: 17,
-18: 19,
-19: 19,
-20: 21,
-21: 20,
-...
-50: 51
-}
+# def divisible_by_2_hash(key_num)
+#   if key_num %2 == 0
+#     return value_num {}<< key_num + 1
+# end
+#
+#
+# def divisible_by_7_hash
+#
+# end
+#
+# def divisible_by_2_and_7_hash
+# end
+#
+#
+# {
+# 1: 1,
+# 2: 3,
+# 3: 3,
+# 4: 5,
+# 5: 5,
+# 6: 7,
+# 7: 6,
+# 8: 9,
+# 9: 9,
+# 10: 11,
+# 11: 11,
+# 12: 13,
+# 13: 13,
+# 14: 28,
+# 15: 15,
+# 16: 17,
+# 17: 17,
+# 18: 19,
+# 19: 19,
+# 20: 21,
+# 21: 20,
+# ...
+# 50: 51
+# }
